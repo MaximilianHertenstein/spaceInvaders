27bc45687307a5a14e4a58395179e1d50f1a3af1
MaximilianHertenstein <hert@ghse.de>
2025-11-08 19:42:59 +0100

    update

--- src/main/java/org/example/HitBox.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public record HitBox(V2 pos, int width, int height) {
-    boolean intersects(HitBox other){
-        boolean intersectsX = pos.x() >= other.pos().x() && pos.x() < other.pos().x() + other.width() || other.pos().x() >= pos.x() && other.pos().x() < pos.x() + width();
-        boolean intersectsY = pos.y() >= other.pos().y() && pos.y() < other.pos().y() + other.height() || other.pos().y() >= pos.y() && other.pos().y() < pos.y() + height();
-        return intersectsX && intersectsY;
-    }
-}
